2f2bd0dbf7d799947712d77003696416fdd08c3c
geerma <geerm1997@gmail.com>
2022-07-21 21:50:24 -0600

    initialize

---
+++ Root.js
@@ -0,0 +1,10 @@
+import React from "react-dom";
+import { View, Text } from "react-native";
+
+export default function Router() {
+  return (
+    <View>
+      <Text>Root</Text>
+    </View>
+  );
+}
